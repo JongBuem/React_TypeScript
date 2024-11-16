31a11c73dba3210423d788610874517d30a81647
JongBuem <whdqja1918@naver.com>
2024-11-16 23:24:40 +0900

    test: schedule.constant.ts 테스트

--- src/common/constants/schedule.constant.ts
+++ src/common/constants/schedule.constant.ts
@@ -9,3 +9,48 @@ export const SCHEDULE_KEY_STARTDATETIME = "startDateTime";
 export const SCHEDULE_KEY_REPEATCYCLEMONTH = "repeatCycleMonth";
 export const SCHEDULE_KEY_REPEATCYCLEWEEK = "repeatCycleWeek";
 export const SCHEDULE_KEY_REPEATCYCLEDAY = "repeatCycleDay";
+
+//as const
+export const scheduleConstant = {
+  SCHEDULE_KEY_ID: "_id",
+  SCHEDULE_KEY_STATE: "useYn",
+  SCHEDULE_KEY_NAME: "scheduleName",
+  SCHEDULE_KEY_SUBSID: "subscriptionId",
+  SCHEDULE_KEY_HOST: "host",
+  SCHEDULE_KEY_TARGETHOSTLIST: "targetHostList",
+  SCHEDULE_KEY_SCHEDULETYPE: "scheduleType",
+  SCHEDULE_KEY_STARTDATETIME: "startDateTime",
+  SCHEDULE_KEY_REPEATCYCLEMONTH: "repeatCycleMonth",
+  SCHEDULE_KEY_REPEATCYCLEWEEK: "repeatCycleWeek",
+  SCHEDULE_KEY_REPEATCYCLEDAY: "repeatCycleDay",
+} as const;
+
+// // enum
+// export enum scheduleConstant {
+//   "_id",
+//   "useYn",
+//   "scheduleName",
+//   "subscriptionId",
+//   "host",
+//   "targetHostList",
+//   "scheduleType",
+//   "startDateTime",
+//   "repeatCycleMonth",
+//   "repeatCycleWeek",
+//   "repeatCycleDay",
+// }
+
+// //const enum
+// export const enum ScheduleConstant {
+//   "_id",
+//   "useYn",
+//   "scheduleName",
+//   "subscriptionId",
+//   "host",
+//   "targetHostList",
+//   "scheduleType",
+//   "startDateTime",
+//   "repeatCycleMonth",
+//   "repeatCycleWeek",
+//   "repeatCycleDay",
+// }
